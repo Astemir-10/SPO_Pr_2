6db3322ceae0d673b13225a2200475bcad90f0e7
ast <shibzuhov2000@mail.ru>
2021-03-13 15:13:45 +0300

    Initial Commit

---
+++ SPO_Pr_2/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  SPO_Pr_2
+//
+//  Created by Astemir Shibzuhov on 3/13/21.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ SPO_Pr_2/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  SPO_Pr_2
+//
+//  Created by Astemir Shibzuhov on 3/13/21.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
